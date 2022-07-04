e992d5511ad32e9c1b494cf0313d20df2f83c8f3
Gux <43382783+MAINcsgo@users.noreply.github.com>
2022-07-04 19:03:43 +0200

    Update DEPOSIT.py

--- DEPOSIT.py
+++ DEPOSIT.py
@@ -2,7 +2,7 @@ from discordwebhook import Discord
 import pyautogui
 
 #DC WEBHOOK (Use your own webhook)
-WEBHOOK = "https://discord.com/api/webhooks/992425649740455937/Qa5IIsnaOHlG56IdfsB23JeoDF65-CKMIJ-18d_q87KI1J_MOwIQ3QAH0tmDqYesIWIF"
+WEBHOOK = "enter webhook here"
 
 #COLORS
 IAM_READY_COLOR = (0,199,77) #BRIGHT GREEN COLOR
