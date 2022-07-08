7c03d56d096649f4c18a58c05eb8094edea13678
Gux <43382783+MAINcsgo@users.noreply.github.com>
2022-07-08 15:31:11 +0200

    Add files via upload

---
+++ PREFIRE.py
@@ -0,0 +1,37 @@
+import time
+import pyautogui as p
+
+WITHDRAW_COLOR = (0,199,77)
+WITHDRAW_REGION = (1843,319,10,7)
+WITHDRAW_BTN = (4,3)
+
+def SolveCaptcha():
+    start_time = time.time()
+    seconds = 3
+
+    while True:
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        
+        p.click(750,620)
+
+        if elapsed_time > seconds:
+            p.click()
+            break 
+    main()
+
+def main():
+    while True:
+        p.click(400,620)
+        
+        im = p.screenshot(region=WITHDRAW_REGION)
+        
+        withdraw_pos = im.getpixel(WITHDRAW_BTN)
+        
+        if withdraw_pos == WITHDRAW_COLOR:
+            p.click(1638,350)
+            SolveCaptcha()
+            break
+           
+print("Running PSYCHO-FAST...")
+main()
